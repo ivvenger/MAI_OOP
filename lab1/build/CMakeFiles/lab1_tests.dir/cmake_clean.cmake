8b1e88797934d597ac93c5ed05b22ea2f55044d7
ivvenger <vengerir@yandex.ru>
2024-11-04 17:45:30 +0300

    Extra information for README.md

---
+++ lab1/build/CMakeFiles/lab1_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_tests.dir/tests/test_lab1.cpp.o"
+  "CMakeFiles/lab1_tests.dir/tests/test_lab1.cpp.o.d"
+  "lab1_tests"
+  "lab1_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
