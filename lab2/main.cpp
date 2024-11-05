7f8d521c023c27cfaaa2e06cbbdd6e3665a491e4
ivvenger <vengerir@yandex.ru>
2024-11-05 14:28:52 +0300

    Create main file and CMake file.

---
+++ lab2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s1, s2;
+    char option;
+    cout << "Введите 1 число" << '\n';
+    cin >> s1;
+    cout << "Введите 2 число" << '\n';
+    cin >> s2;
+    cout << "Доступные опции: +,-,=" << '\n';
+    cout << "Введите опцию" << '\n';
+    cin >> option;
+}
