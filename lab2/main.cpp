aefda8b2d53e28a7f5a02c1b98ce8ddf8fbdfa16
ivvenger <vengerir@yandex.ru>
2024-11-18 18:57:17 +0300

    Main file creation.

---
+++ lab2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s1, s2;
+    // char opt;
+    cout << "Введите первое число" << '\n';
+    cin >> s1;
+    cout << "Введите второе число" << '\n';
+    cin >> s2;
+    cout << "Введите операцию из списка: +, -, >, <, =" << '\n';
+    cout << "Бум бля";
+}
